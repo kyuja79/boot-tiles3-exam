5d400fed84f30181cf1f8b425923d4baf8fa715c
kyuja79 <kyuja79@gmail.com>
2017-08-28 10:23:50 +0900

    init boot-tiles3-exam project

---
+++ boot-tiles3-exam/src/main/java/com/gaebalja/boottiles3exam/BootTiles3ExamApplication.java
@@ -0,0 +1,12 @@
+package com.gaebalja.boottiles3exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootTiles3ExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootTiles3ExamApplication.class, args);
+	}
+}
---
+++ boot-tiles3-exam/src/test/java/com/gaebalja/boottiles3exam/BootTiles3ExamApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gaebalja.boottiles3exam;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BootTiles3ExamApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
