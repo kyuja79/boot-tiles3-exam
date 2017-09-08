914587cc480e1af643ff6f543d6a935634b4c16b
kyuja79 <kyuja79@gmail.com>
2017-09-08 11:23:07 +0900

    add webjars resourceHandler

--- boot-tiles3-exam/src/main/java/com/gaebalja/boottiles3exam/config/WebConfiguration.java
+++ boot-tiles3-exam/src/main/java/com/gaebalja/boottiles3exam/config/WebConfiguration.java
@@ -19,6 +19,7 @@ public class WebConfiguration extends WebMvcConfigurerAdapter {
 	@Override
 	public void addResourceHandlers(final ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
 	}
 	
 	@Bean
